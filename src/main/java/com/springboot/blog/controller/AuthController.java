f07de3c43a2971494d350e78621c0fd851022436
FNU Shaliwa <sf452@cornell.edu>
2024-03-28 07:44:37 -0400

    Secure REST API's with JWT Token

--- src/main/java/com/springboot/blog/controller/AuthController.java
+++ src/main/java/com/springboot/blog/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.springboot.blog.controller;
 
+import com.springboot.blog.payload.JWTAuthResponse;
 import com.springboot.blog.payload.LoginDto;
 import com.springboot.blog.payload.RegisterDto;
 import com.springboot.blog.service.AuthService;
@@ -20,10 +21,12 @@ public class AuthController {
 
     // Build Login REST API
     @PostMapping(value = {"/login", "/signin"}) // configure multiple urls for api
-    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
-        String response = authService.login(loginDto);
-        return ResponseEntity.ok(response);
-    }
+    public ResponseEntity<JWTAuthResponse> login(@RequestBody LoginDto loginDto) {
+        String token = authService.login(loginDto);
+        JWTAuthResponse jwtAuthResponse = new JWTAuthResponse();
+        jwtAuthResponse.setAccessToken(token);
+        return ResponseEntity.ok(jwtAuthResponse);
+    } // modified login api to return JWT token in the response
 
     // Build register REST API
     @PostMapping(value = {"/register", "/signup"})
